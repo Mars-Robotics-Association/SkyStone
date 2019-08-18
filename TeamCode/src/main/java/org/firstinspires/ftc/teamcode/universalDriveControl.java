abcacd5ea11aee137fce00cce1cca8fdea9263b3
Isaac Thoman <isaacthoman@outlook.com>
2019-08-18 19:33:35 -0400

    Added universal drive control

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/universalDriveControl.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+
+
+
+public class universalDriveControl {
+    double a;
+    a = gamepad1.right_stick_y;;
+
+}
+
