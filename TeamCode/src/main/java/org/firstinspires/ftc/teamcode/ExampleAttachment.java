72a50a2b34f0b8060ca168c0d9bd02f5e81acfac
Owen Anderson <owencanderson@icloud.com>
2019-08-18 19:56:43 -0400

    Added code for attachment infastructure with example script

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ExampleAttachment.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+/*
+This is an example script that outlines an idea for managing attachments in a clean manner the Run()
+method probably need to run in the Loop().
+ */
+public class ExampleAttachment extends Attachment {
+
+    @Override
+    void Run()
+    {
+        //Put attachment code here for running
+    }
+
+    @Override
+    void Stop()
+    {
+        //Put code for stopping here
+    }
+}
