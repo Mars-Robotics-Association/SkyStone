c92ef0402866b6d5c59ecb08948c7ad2d13dc5c0
Owen Anderson <owencanderson@icloud.com>
2019-08-18 20:14:29 -0400

    Updated Attachments.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Attachment.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Attachment.java
@@ -5,7 +5,7 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 /*
 Parent class for all attachments.
 DO NOT EDIT UNLESS YOU KNOW WHAT YOU ARE DOING!!! You might break all attachment child classes...
-Then we would need a funeral :( 
+Then we would need a funeral :(
  */
 
 public class Attachment {
