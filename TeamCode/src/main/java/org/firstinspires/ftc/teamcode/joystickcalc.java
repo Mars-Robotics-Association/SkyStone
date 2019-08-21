c3754c8852d1347fab6cabdcfa4868afbe76d106
IsaacThoman <isaacthoman@outlook.com>
2019-08-20 18:34:09 -0700

    Testing out pass by reference

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/joystickcalc.java
@@ -0,0 +1,53 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+
+//@Disabled
+
+/**
+ * Created by Matthew on 9/20/2018.
+ */
+
+public class joystickcalc {
+
+    private ElapsedTime runtime = new ElapsedTime();
+
+
+
+    private LinearOpMode lin_opmode;
+
+    public joystickcalc(Telemetry telemetry, HardwareMap hardwareMap, LinearOpMode linopmode)   {
+
+        lin_opmode=linopmode;
+        runtime.reset();
+    }
+
+    public void GoForward(double inches) {
+
+
+
+    }}
