72fccc9749253eeb7f1f258e28acedb30bf700c6
IsaacThoman <isaacthoman@outlook.com>
2019-08-21 11:45:03 -0700

    I'm now able to get joystick data from another class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JoystickCalc.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class JoystickCalc
+{
+    private OpMode opmode;
+
+    double leftStickY;
+    double leftStickX;
+    double rightStickX;
+    double rightStickY;
+    boolean xButton;
+    boolean yButton;
+    boolean aButton;
+    boolean bButton;
+
+    public JoystickCalc(OpMode opmode)
+    {
+        this.opmode = opmode;
+    }
+
+    public void calculate ()
+    {
+        leftStickY = opmode.gamepad1.left_stick_y;
+        leftStickX = opmode.gamepad1.left_stick_x;
+        rightStickX = opmode.gamepad1.right_stick_x;
+        rightStickY = opmode.gamepad1.right_stick_y;
+        xButton = opmode.gamepad1.x;
+        yButton = opmode.gamepad1.y;
+        bButton = opmode.gamepad1.b;
+        aButton = opmode.gamepad1.a;
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UDCTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UDCTest.java
@@ -2,51 +2,25 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.hardware.TouchSensor;
-import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.hardware.DigitalChannel;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DigitalChannel;
 
-
-@TeleOp(name="Universal Drive Control PBR Test", group="Iterative Opmode")
-//@Disabled
-
-public class UDCTest extends OpMode {
-
-  //  private ElapsedTime runtime = null;
-   joystickcalc joyCalc = new joystickcalc();
-   
-//     joystickcalc joyCalc = null;
+@TeleOp
+public class UDCTest extends OpMode
+{
+    JoystickCalc joystickCalc;
     double test1;
-    public void init() {
-
-    }
 
     @Override
-    public void init_loop() {
-
+    public void init()
+    {
+        joystickCalc = new JoystickCalc(this);
     }
-    @Override
-    public void start() {
-
-
-    }
-
 
     @Override
     public void loop() {
-//committed from git desktop 4
-joyCalc.calculate();
-test1 = joyCalc.leftStickX;
-        //telemetry.addData("X", "joyCalc.leftStickX");
+
+        joystickCalc.calculate();
+        test1 = joystickCalc.leftStickX;
+        telemetry.addData("X", test1);
         telemetry.update();
     }
-
-
 }
