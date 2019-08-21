4299ea155f32475feb7bc793494d4e7851099131
IsaacThoman <isaacthoman@outlook.com>
2019-08-21 11:07:23 -0700

    variable comment test 3

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UDCTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UDCTest.java
@@ -41,7 +41,7 @@ public class UDCTest extends OpMode {
 
     @Override
     public void loop() {
-//committed from git desktop 22
+//committed from git desktop 4
 joyCalc.calculate();
 test1 = joyCalc.leftStickX;
         //telemetry.addData("X", "joyCalc.leftStickX");
