58ccfa738d9c99feea85a260e556f537e8726f0a
Isaac Thoman <isaacthoman@outlook.com>
2019-08-22 20:43:43 -0400

    Defined Quadrants in joystickCalc

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JoystickCalc.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JoystickCalc.java
@@ -31,5 +31,52 @@ public class JoystickCalc
         yButton = opmode.gamepad1.y;
         bButton = opmode.gamepad1.b;
         aButton = opmode.gamepad1.a;
+
+if(leftStickY > 0){
+//quadrants A and Dif()
+          if(leftStickX > 0){
+
+              
+
+
+            }
+          if(leftStickX < 0){
+              //quadrant D
+          }
+          if(leftStickX == 0){
+              //along y axis
+          }
+}
+if(leftStickY < 0){
+//quadrants C and B
+        if(leftStickX > 0){
+                //quadrant B
+        }
+        if(leftStickX < 0){
+                //quadrant C
+       }
+        if(leftStickX == 0){
+             //along y axis
+      }
+
+}
+if(leftStickY == 0){
+//along x axis
+    if(leftStickX > 0){
+        //along right x axis
     }
+    if(leftStickX < 0){
+        //along left x axis
+    }
+    if(leftStickX == 0){
+        //in the center
+    }
+
+
+}
+
+    }
+
+
+
 }
