2eb24a0ea9beb896042ec7b5b9a316979b9d266d
Owen Anderson <owencanderson@icloud.com>
2019-08-25 20:38:54 -0400

    Put together basic code for mecanum drive control

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UDCTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UDCTest.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
+import android.opengl.Matrix;
+
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
@@ -36,11 +38,23 @@ public class UDCTest extends OpMode
         telemetry.update();
 
         robotAngle = vuforia.RobotAngle;
-        
     }
 
     double GetRelativeDegrees(double JoystickAngle, double RobotAngle)
     {
         return (JoystickAngle - RobotAngle);
+    }
+
+    void CalculateWheelSpeeds(double degrees)
+    {
+        double FrontRight = 0;
+        double FrontLeft = 0;
+        double RearRight = 0;
+        double RearLeft = 0;
+
+        FrontRight = Math.cos(Math.toRadians(degrees + 45));
+        FrontLeft = Math.cos(Math.toRadians(degrees - 45));
+        RearRight = Math.cos(Math.toRadians(degrees - 45));
+        RearLeft = Math.cos(Math.toRadians(degrees + 45));
     }
 }
