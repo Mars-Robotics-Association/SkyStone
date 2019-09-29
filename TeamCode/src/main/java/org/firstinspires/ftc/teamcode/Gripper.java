03dd563b905bbc31386c6b435463ef84f5b1a778
IsaacThoman <isaacthoman@outlook.com>
2019-09-29 19:24:08 -0400

    Create Gripper.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Gripper.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode;
+
+public class Gripper implements Attachment{
+    @Override
+    public void Init() {
+
+    }
+
+    @Override
+    public void Loop() {
+
+    }
+
+    @Override
+    public void Run() {
+
+    }
+
+    @Override
+    public void Stop() {
+
+    }
+}
