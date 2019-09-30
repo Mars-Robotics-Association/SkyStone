b7bda585b17ded08af78a17ba5c8bd3e80be74bf
Isaac Thoman <isaacthoman@outlook.com>
2019-09-29 20:38:41 -0400

    Finished Gripper code, started work on Teleop class to test it.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GripperTeleOpTest.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="GripperTeleOpTest", group="Iterative Opmode")
+public class GripperTeleOpTest extends OpMode
+{
+
+    @Override
+    public void init()
+    {
+
+    }
+
+    @Override
+    public void start()
+    {
+
+
+    }
+
+    @Override
+    public void loop()
+    {
+
+    }
+}
