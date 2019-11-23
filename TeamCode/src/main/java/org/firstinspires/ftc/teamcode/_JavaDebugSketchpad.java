c099164b4bf0923b644117a987e1953db2ef5935
Owen <owencanderson@icloud.com>
2019-11-22 20:12:01 -0500

    Debugged Vuforia and got angles right

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/_JavaDebugSketchpad.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/_JavaDebugSketchpad.java
@@ -2,10 +2,10 @@ package org.firstinspires.ftc.teamcode;
 
 public class _JavaDebugSketchpad
 {
-    private static double TargetX = -36;
+    private static double TargetX = 36;
     private static double TargetY = 36;
     private static double CurrentX = -36;
-    private static double CurrentY = -36;
+    private static double CurrentY = 36;
     private static double closeEnoughThresholdDist = 5;
     private static double StartAngle = 180;
     private static double absoluteAngle = 0;
@@ -14,11 +14,13 @@ public class _JavaDebugSketchpad
     public static void main(String[] args) 
     {
         //Calculate angle of movement (no obstacle avoidance)
-        double triY = (CurrentY + TargetY); //vertical length
-        double triX = (CurrentX + TargetX); //horizontal length
+        double triY = (CurrentY - TargetY); //vertical length
+        double triX = (CurrentX - TargetX); //horizontal length
         absoluteAngle = Math.atan2(triY,triX); //get measurement of move angle
         absoluteAngle = Math.toDegrees(absoluteAngle);
-        absoluteAngle -= StartAngle; //offsets by start angle of the robot
+        System.out.println(absoluteAngle);
+        absoluteAngle -= StartAngle + 180; //offsets by start angle of the robot
+        System.out.println(absoluteAngle);
         if(absoluteAngle < 0)//convert degrees to positive if needed
         {
             absoluteAngle = 360 + absoluteAngle;
@@ -28,16 +30,16 @@ public class _JavaDebugSketchpad
         System.out.println(triX);
     }
 
-    //  //Calculate angle of movement (no obstacle avoidance)
-    //  double triY = Math.abs(CurrentY + TargetY); //vertical length
-    //  double triX = Math.abs(CurrentX + TargetX); //horizontal length
-    //  absoluteAngle = Math.atan2(triY,triX); //get measurement of joystick angle
-    //  absoluteAngle = Math.toDegrees(absoluteAngle);
-    //  absoluteAngle -= StartAngle;
-    //  if(absoluteAngle < 0)//convert degrees to positive if needed
-    //  {
-    //      absoluteAngle = 360 + absoluteAngle;
-    //  }
+    // //Calculate angle of movement
+    // double triY = (CurrentY - TargetY); //vertical length
+    // double triX = (CurrentX - TargetX); //horizontal length
+    // absoluteAngle = Math.atan2(triY,triX); //get measurement of move angle
+    // absoluteAngle = Math.toDegrees(absoluteAngle);
+    // absoluteAngle -= StartAngle; //offsets by start angle of the robot
+    // if(absoluteAngle < 0)//convert degrees to positive if needed
+    // {
+    //     absoluteAngle = 360 + absoluteAngle;
+    // }
 }
 
 
