7c9c00b976e67c299518714223053854a726f56d
IsaacThoman <isaacthoman@outlook.com>
2019-11-26 20:44:42 -0500

    t e s t i n g   s t u f f

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/depotSingleBackup.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/depotSingleBackup.java
@@ -26,47 +26,11 @@ public class depotSingleBackup extends LinearOpMode {
 
     @Override
     public void runOpMode() {
-        FrontRight = hardwareMap.get(DcMotor.class, "FrontRight");
-        FrontLeft = hardwareMap.get(DcMotor.class, "FrontLeft");
-        RearRight = hardwareMap.get(DcMotor.class, "RearRight");
-        RearLeft = hardwareMap.get(DcMotor.class, "RearLeft");
-
-        FrontLeft.setDirection(DcMotor.Direction.REVERSE); // Set to REVERSE if using AndyMark motors
-        RearLeft.setDirection(DcMotor.Direction.REVERSE); // Set to REVERSE if using AndyMark motors
-        FrontRight.setDirection(DcMotor.Direction.FORWARD);
-        RearRight.setDirection(DcMotor.Direction.FORWARD);
-
-        FrontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        FrontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        RearLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        RearRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
-        FrontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        FrontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        RearRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        RearLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-        FrontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        FrontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        RearLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        RearRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
 
 
-        FrontLeft.setPower(Math.abs(0.5));
-        FrontRight.setPower(Math.abs(0.5));
-        RearLeft.setPower(Math.abs(0.5));
-        RearRight.setPower(Math.abs(0.5));
 
-        sleep(3000);
-
-        FrontLeft.setPower(0);
-        FrontRight.setPower(0);
-        RearLeft.setPower(0);
-        RearRight.setPower(0);
-
-//GPG
-
+        skystonebot.MoveAtAngle(0,1,false);
 
 
     }
