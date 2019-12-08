f65a3d3b130c4287e3ff0e9c9effc6c627547cae
Owen <owencanderson@icloud.com>
2019-12-07 20:25:41 -0500

    updated java debug

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/_JavaDebugSketchpad.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/_JavaDebugSketchpad.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
+import org.graalvm.compiler.core.common.FieldsScanner.CalcOffset;
+
 public class _JavaDebugSketchpad
 {
     private static double TargetX = 36;
@@ -14,11 +16,62 @@ public class _JavaDebugSketchpad
     private static double WheelDiameter = 2;//diameter of wheel in inches
     private static int encodedDistance = 0;
 
+    static double leftStickY = 0;
+    static double leftStickX = 0;
+    static double leftStickBaring = 0;
+    static double leftStickPower = 0;
+    static double rightStickX = 0;
+    static double rightStickY = 0;
+    static double rightStickBaring = 0;
+    static double rightStickPower = 0;
+    static boolean xButton;
+    static boolean yButton;
+    static boolean aButton;
+    static boolean bButton;
+    static float leftJS = 0;
+    static float rightJS = 0;
+
+    private double rotationOffset = 0;
+
 
     public static void main(String[] args) 
     {
-        encodedDistance = (int)((EncoderTicks/WheelDiameter)*25);
-        System.out.println(encodedDistance);
+        calculate();
+        
+
+        System.out.println(leftStickBaring);
+        System.out.println(leftStickPower);
+
+    }
+
+    public static void calculate ()
+    {
+
+        leftStickY = 0;
+        leftStickX = -1;
+        rightStickX = 0;
+        rightStickY = 0;
+        xButton = false;
+        yButton =false ;
+        bButton = false;
+        aButton = false;
+        leftJS = 0;
+        rightJS = 0;
+
+
+        //Calculate angle of joystick
+        double Y = leftStickY; //X input
+        double X = leftStickX; //Y input
+        leftStickBaring = Math.atan2(Y,-X); //get measurement of joystick angle
+        leftStickBaring = Math.toDegrees(leftStickBaring);
+        leftStickBaring -= 90 - 180;
+        if(leftStickBaring < 0)//convert degrees to positive if needed
+        {
+            leftStickBaring = 360 + leftStickBaring;
+        }
+        //Distance formula for calculating joystick power
+        leftStickPower = Math.abs(Math.sqrt(Math.pow(leftStickX - 0, 2) + Math.pow(leftStickY - 0, 2)));
+
     }
 
     // //Calculate angle of movement
