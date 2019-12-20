5d4f013605fba0fb7a57c6d165893f44bf83178e
IsaacThoman <isaacthoman@outlook.com>
2019-12-19 20:22:32 -0500

    R O H A N

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/rohan.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(name = "Rohan's sentient Ender Scroll v special edition", group = "Autonomous")
+public class rohan {
+
+private SkyStoneBot robot;
+
+
+
+    public void runOpMode() {
+
+
+        robot.RawForwards(1);
+
+
+
+    }
+
+
+
+
+}
