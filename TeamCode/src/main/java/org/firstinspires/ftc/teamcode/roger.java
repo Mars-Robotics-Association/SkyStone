f7656fc60259a83c4116f67e3eec590c5a3722b8
IsaacThoman <isaacthoman@outlook.com>
2020-01-09 20:43:59 -0500

    fixed errors?

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roger.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roger.java
@@ -13,73 +13,15 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 
 public class roger  {
 
-    // List of available sound resources
-    String  sounds[] =  {"ss_alarm", "ss_bb8_down", "ss_bb8_up", "ss_darth_vader", "ss_fly_by",
-            "ss_mf_fail", "ss_laser", "ss_laser_burst", "ss_light_saber", "ss_light_saber_long", "ss_light_saber_short",
-            "ss_light_speed", "ss_mine", "ss_power_up", "ss_r2d2_up", "ss_roger_roger", "ss_siren", "ss_wookie" };
-    boolean soundPlaying = false;
+
 
     @Override
     public void runOpMode() {
 
-        // Variables for choosing from the available sounds
-        int     soundIndex      = 0;
-        int     soundID         = -1;
-        boolean was_dpad_up     = false;
-        boolean was_dpad_down   = false;
-
-        Context myApp = hardwareMap.appContext;
-
-        // create a sound parameter that holds the desired player parameters.
-        SoundPlayer.PlaySoundParams params = new SoundPlayer.PlaySoundParams();
-        params.loopControl = 0;
-        params.waitForNonLoopingSoundsToFinish = true;
-
-        // In this sample, we will skip waiting for the user to press play, and start displaying sound choices right away
-        while (!isStopRequested()) {
-
-            // Look for DPAD presses to change the selection
-            if (gamepad1.dpad_down && !was_dpad_down) {
-                // Go to next sound (with list wrap) and display it
-                soundIndex = (soundIndex + 1) % sounds.length;
-            }
-
-            if (gamepad1.dpad_up && !was_dpad_up) {
-                // Go to previous sound (with list wrap) and display it
-                soundIndex = (soundIndex + sounds.length - 1) % sounds.length;
-            }
-
-            // Look for trigger to see if we should play sound
-            // Only start a new sound if we are currently not playing one.
-            if (gamepad1.right_bumper && !soundPlaying) {
-
-                // Determine Resource IDs for the sounds you want to play, and make sure it's valid.
-                if ((soundID = myApp.getResources().getIdentifier(sounds[soundIndex], "raw", myApp.getPackageName())) != 0){
-
-                    // Signal that the sound is now playing.
-                    soundPlaying = true;
-
-                    // Start playing, and also Create a callback that will clear the playing flag when the sound is complete.
-                    SoundPlayer.getInstance().startPlaying(myApp, soundID, params, null,
-                            new Runnable() {
-                                public void run() {
-                                    soundPlaying = false;
-                                }} );
                 }
             }
 
-            // Remember the last state of the dpad to detect changes.
-            was_dpad_up     = gamepad1.dpad_up;
-            was_dpad_down   = gamepad1.dpad_down;
 
-            // Display the current sound choice, and the playing status.
-            telemetry.addData("", "Use DPAD up/down to choose sound.");
-            telemetry.addData("", "Press Right Bumper to play sound.");
-            telemetry.addData("", "");
-            telemetry.addData("Sound >", sounds[soundIndex]);
-            telemetry.addData("Status >", soundPlaying ? "Playing" : "Stopped");
-            telemetry.update();
-        }
 
 public void roger {
             String  sounds[] =  {"ss_alarm", "ss_bb8_down", "ss_bb8_up", "ss_darth_vader", "ss_fly_by",
