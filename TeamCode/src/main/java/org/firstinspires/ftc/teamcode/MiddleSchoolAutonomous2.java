6e4bb829207f431bac80ceaece7560e95e86efcb
isaacthoman <isaacthoman@outlook.com>
2020-01-19 19:48:11 -0500

    disabled autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MiddleSchoolAutonomous2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MiddleSchoolAutonomous2.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-
+@Disabled
 @Autonomous(name = "MS Simple", group = "Autonomous")
 public class MiddleSchoolAutonomous2 extends LinearOpMode {
 
