41b257fa7cc7e5bec4b7c6068dbe523a0df79809
Rohan <rohan@beanchat.tk>
2020-02-23 15:09:22 -0500

    Revert "specify servo direction"
    This reverts commit cc0749c51195f1d93db5bf2924c8b1a42e30751b.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTest.java
@@ -37,7 +37,6 @@ public class ServoTest extends OpMode
 
         if(gamepad1.a){
             GripperL.setPosition(0.3);
-            GripperL.setDirection(1);
         }
         if(gamepad1.b){GripperL.setPosition(0.5);}
         telemetry.update();
